3dbd1532469c685ca948d1d58556983ed18c5a24
Henrique <henriquewl@gmail.com>
2023-01-23 19:46:22 -0300

    Simple Runelite plugin example

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -1,53 +1,37 @@
 package com.example;
 
-import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigManager;
+import net.runelite.api.widgets.WidgetID;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.api.events.WidgetLoaded;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+		name = "Runelite Example Plugin"
 )
 public class ExamplePlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
-	@Inject
-	private ExampleConfig config;
-
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
-	}
-
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
-	}
-
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onWidgetLoaded(WidgetLoaded widgetLoaded)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		if (widgetLoaded.getGroupId() == WidgetID.BANK_GROUP_ID)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
+			final Widget bankTutorialButtonWidget = client.getWidget(WidgetInfo.BANK_TUTORIAL_BUTTON);
+			log.info(String.valueOf(bankTutorialButtonWidget));
 
-	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(ExampleConfig.class);
+			if (bankTutorialButtonWidget != null && !bankTutorialButtonWidget.isSelfHidden())
+			{
+				bankTutorialButtonWidget.setHidden(true);
+			}
+		}
 	}
 }
